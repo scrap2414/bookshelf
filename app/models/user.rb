d4bcd157a5833208a050b0466b1cf20b086f6aea
scrap2414 <80100413+scrap2414@users.noreply.github.com>
2021-04-20 18:50:17 +0900

    ユーザーログインの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :name, presence: true
+         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, 
+          message: 'は英語と数字の両方を含めて設定してください' }       
 end
