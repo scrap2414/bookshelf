f83d02591c599814117cb7811255c406cdbc2d20
scrap2414 <80100413+scrap2414@users.noreply.github.com>
2021-04-21 19:11:40 +0900

    ActiveStorageの導入

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,15 +1,15 @@
 class Category < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: 'レディース' },
-    { id: 3, name: 'メンズ' },
-    { id: 4, name: 'ベビー・キッズ' },
-    { id: 5, name: 'インテリア・住まい・小物' },
-    { id: 6, name: '本・音楽・ゲーム' },
-    { id: 7, name: 'おもちゃ・ホビー・グッズ' },
-    { id: 8, name: '家電・スマホ・カメラ' },
-    { id: 9, name: 'スポーツ・レジャー' },
-    { id: 10, name: 'ハンドメイド' },
+    { id: 2, name: '現代小説' },
+    { id: 3, name: '恋愛小説' },
+    { id: 4, name: '推理小説' },
+    { id: 5, name: 'SF・ホラー' },
+    { id: 6, name: 'ノベライズ' },
+    { id: 7, name: 'ビジネス' },
+    { id: 8, name: '趣味' },
+    { id: 9, name: '雑学' },
+    { id: 10, name: 'アート' },
     { id: 11, name: 'その他' }
   ]
 
