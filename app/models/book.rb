e55edefa959dd522353d03f438571460336653c8
scrap2414 <80100413+scrap2414@users.noreply.github.com>
2021-04-22 12:51:52 +0900

    unicorn実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-
   belongs_to :category
   belongs_to :user
+  has_one_attached :image, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many  :items  
+  has_one_attached :image
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
