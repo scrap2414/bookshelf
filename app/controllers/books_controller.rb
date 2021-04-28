c9f8a10ad6b67c2cb69a6fe39757b6b13b99ef25
scrap2414 <80100413+scrap2414@users.noreply.github.com>
2021-04-28 19:40:18 +0900

    book詳細ページの表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,6 +19,11 @@ class BooksController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+
   def edit
   end
 
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,11 +42,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def update_resource(resource, params)
     resource.update_without_password(params)
   end
-
-  #任意  更新後のパスを指定
+ 
   def after_update_path_for(resource)
     user_path(@user.id)
   end
+
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
